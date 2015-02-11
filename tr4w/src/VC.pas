a8203459afdd61a07b453e6d7b88919a69cf87e1
Howard Hoyt <n4af@n4af.net>
2015-02-10 21:17:39 -0500

    Update VC to 4.37.0

--- tr4w/src/VC.pas
+++ tr4w/src/VC.pas
@@ -157,10 +157,10 @@ const
 
 
   OZCR2008                              = False;
-  TR4W_CURRENTVERSION_NUMBER            = '4.36.13'  ;
+  TR4W_CURRENTVERSION_NUMBER            = '4.37.0'  ;
   TR4W_CURRENTVERSION                   = 'TR4W v.' + TR4W_CURRENTVERSION_NUMBER;//{$IF LANG <> 'ENG'} + ' [' + LANG + ']'{$IFEND}{$IF MMTTYMODE} + '_mmtty'{$IFEND};
 
-  TR4W_CURRENTVERSIONDATE               = 'Feb 9 2015'   ;
+  TR4W_CURRENTVERSIONDATE               = 'Feb 10 2015'   ;
   TR4WSERVER_CURRENTVERSION             = '1.41';
 
   LOGVERSION1                           = 'v';
