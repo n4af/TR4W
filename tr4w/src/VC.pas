258e4769e4656bcc5eb513317a0126b9be2f5eaa
Howard Hoyt <n4af@n4af.net>
2015-09-27 14:59:55 -0400

    4.41.8

--- tr4w/src/VC.pas
+++ tr4w/src/VC.pas
@@ -177,11 +177,11 @@ const
 
 
   OZCR2008                              = False;
-  TR4W_CURRENTVERSION_NUMBER            = '4.41.7'; // n4af
+  TR4W_CURRENTVERSION_NUMBER            = '4.41.8'; // n4af
 
   TR4W_CURRENTVERSION                   = 'TR4W v.' + TR4W_CURRENTVERSION_NUMBER;//{$IF LANG <> 'ENG'} + ' [' + LANG + ']'{$IFEND}{$IF MMTTYMODE} + '_mmtty'{$IFEND};
 
-  TR4W_CURRENTVERSIONDATE               = 'September 21, 2015' ;
+  TR4W_CURRENTVERSIONDATE               = 'September 22, 2015' ;
   TR4WSERVER_CURRENTVERSION             = '1.41';
 
   LOGVERSION1                           = 'v';
