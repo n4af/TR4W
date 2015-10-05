00096ed274a32cbb02ca5145802403c2bb7f6545
Howie/N4AF <howie.n4af@gmail.com>
2015-10-05 15:38:03 -0400

    correct typo P8 DOM file 4.42.3

--- tr4w/src/VC.pas
+++ tr4w/src/VC.pas
@@ -177,11 +177,11 @@ const
 
 
   OZCR2008                              = False;
-  TR4W_CURRENTVERSION_NUMBER            = '4.42.2'; // n4af
+  TR4W_CURRENTVERSION_NUMBER            = '4.42.3'; // n4af
 
   TR4W_CURRENTVERSION                   = 'TR4W v.' + TR4W_CURRENTVERSION_NUMBER;//{$IF LANG <> 'ENG'} + ' [' + LANG + ']'{$IFEND}{$IF MMTTYMODE} + '_mmtty'{$IFEND};
 
-  TR4W_CURRENTVERSIONDATE               = 'Oct 2, 2015' ;
+  TR4W_CURRENTVERSIONDATE               = 'Oct 5, 2015' ;
   TR4WSERVER_CURRENTVERSION             = '1.41';
 
   LOGVERSION1                           = 'v';
