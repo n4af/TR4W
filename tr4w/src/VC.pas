131ce3aefd3a328d419716129981f3eaea7980e2
Howie/N4AF <howie.n4af@gmail.com>
2015-11-03 08:56:49 -0500

    4.42.9

--- tr4w/src/VC.pas
+++ tr4w/src/VC.pas
@@ -177,7 +177,7 @@ const
 
 
   OZCR2008                              = False;
-  TR4W_CURRENTVERSION_NUMBER            = '4.42.Beta'; // n4af
+  TR4W_CURRENTVERSION_NUMBER            = '4.42.9'; // n4af
 
   TR4W_CURRENTVERSION                   = 'TR4W v.' + TR4W_CURRENTVERSION_NUMBER;//{$IF LANG <> 'ENG'} + ' [' + LANG + ']'{$IFEND}{$IF MMTTYMODE} + '_mmtty'{$IFEND};
 
