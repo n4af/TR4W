0287d9789b3b7775d30204f543df25cf773fe80f
Howie/N4AF <howie.n4af@gmail.com>
2015-11-05 08:29:42 -0500

    modified:   tr4w/src/MainUnit.pas 	modified:   tr4w/src/VC.pas 	modified:   tr4w/src/trdos/LOGSUBS1.PAS

--- tr4w/src/VC.pas
+++ tr4w/src/VC.pas
@@ -181,7 +181,7 @@ const
 
   TR4W_CURRENTVERSION                   = 'TR4W v.' + TR4W_CURRENTVERSION_NUMBER;//{$IF LANG <> 'ENG'} + ' [' + LANG + ']'{$IFEND}{$IF MMTTYMODE} + '_mmtty'{$IFEND};
 
-  TR4W_CURRENTVERSIONDATE               = 'Nov 3, 2015' ;
+  TR4W_CURRENTVERSIONDATE               = 'Nov 5, 2015' ;
   TR4WSERVER_CURRENTVERSION             = '1.41';
 
   LOGVERSION1                           = 'v';
