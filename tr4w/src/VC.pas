b6d39d11700a454edf7de2a5b65240c38cd906c8
Howie/N4AF <howie.n4af@gmail.com>
2015-12-21 07:00:07 -0500

    add VC.pas 4.39.9

--- tr4w/src/VC.pas
+++ tr4w/src/VC.pas
@@ -177,11 +177,11 @@ const
 
 
   OZCR2008                              = False;
-  TR4W_CURRENTVERSION_NUMBER            = '4.43.7';  // n4af
+  TR4W_CURRENTVERSION_NUMBER            = '4.43.9';  // n4af
 
   TR4W_CURRENTVERSION                   = 'TR4W v.' + TR4W_CURRENTVERSION_NUMBER;//{$IF LANG <> 'ENG'} + ' [' + LANG + ']'{$IFEND}{$IF MMTTYMODE} + '_mmtty'{$IFEND};
 
-  TR4W_CURRENTVERSIONDATE               = 'Dec 5, 2015' ;
+  TR4W_CURRENTVERSIONDATE               = 'Dec 19, 2015' ;
   TR4WSERVER_CURRENTVERSION             = '1.41';
 
   LOGVERSION1                           = 'v';
