c523a451350fc64b22919e06253d1f4d27da2fec
Howie/N4AF <howie.n4af@gmail.com>
2015-12-30 13:46:54 -0500

    4.44.1 Spring-Arktika formatting corrections

--- tr4w/src/VC.pas
+++ tr4w/src/VC.pas
@@ -177,11 +177,11 @@ const
 
 
   OZCR2008                              = False;
-  TR4W_CURRENTVERSION_NUMBER            = '4.43.11';  // n4af
+  TR4W_CURRENTVERSION_NUMBER            = '4.44.0';  // n4af
 
   TR4W_CURRENTVERSION                   = 'TR4W v.' + TR4W_CURRENTVERSION_NUMBER;//{$IF LANG <> 'ENG'} + ' [' + LANG + ']'{$IFEND}{$IF MMTTYMODE} + '_mmtty'{$IFEND};
 
-  TR4W_CURRENTVERSIONDATE               = 'Dec 28, 2015' ;
+  TR4W_CURRENTVERSIONDATE               = 'Dec 30, 2015' ;
   TR4WSERVER_CURRENTVERSION             = '1.41';
 
   LOGVERSION1                           = 'v';
