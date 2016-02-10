1a945f60c8066c5032765d0725d1ff3d4d7a574b
Howie/N4AF <howie.n4af@gmail.com>
2016-02-09 22:27:57 -0500

    CWBYCATSO2R beta test

--- tr4w/src/VC.pas
+++ tr4w/src/VC.pas
@@ -176,7 +176,7 @@ const
 
   OZCR2008                              = False;
 
-  TR4W_CURRENTVERSION_NUMBER            = '4.45.8'; // N4AF
+  TR4W_CURRENTVERSION_NUMBER            = '4.45.cwbycatso2r' ; // N4AF
 
   TR4W_CURRENTVERSION                   = 'TR4W v.' + TR4W_CURRENTVERSION_NUMBER;//{$IF LANG <> 'ENG'} + ' [' + LANG + ']'{$IFEND}{$IF MMTTYMODE} + '_mmtty'{$IFEND};
 
