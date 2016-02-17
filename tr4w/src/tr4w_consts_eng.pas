b2673c6ff1881a6a424469348f50f8fef7d043db
Tom Schaefer <toms@bettersoftwaresolutions.com>
2016-02-17 02:27:35 -0500

    Closes issue #28, #29, and #30. This patch handles if the DX station exchange is not DX. A descriptive error is displayed to the user.

--- tr4w/src/tr4w_consts_eng.pas
+++ tr4w/src/tr4w_consts_eng.pas
@@ -262,6 +262,7 @@ const
   TC_FILESAVEDTOSUCCESSFULLY            = 'File saved to %s successfully.';
   TC_IMPROPERTRANSMITTERCOUNT           = 'FD transmitters must be between 1 and 99.';
   TC_IMPROPERARRLFIELDDAYCLASS          = 'Field Day class must be A, B, C, D, E or F.';
+  TC_ARRLFIELDDAYIMPROPERDXEXCHANGE     = 'DX Station exchange must be "DX".';
 
   {LOGSEND}
 
