5ae4699860ce38b91937db5bfe9e9232015e26b8
Howie/N4AF <howie.n4af@gmail.com>
2016-02-25 08:33:29 -0500

    4.46.4 issue 70

--- tr4w/src/VC.pas
+++ tr4w/src/VC.pas
@@ -177,11 +177,11 @@ const
 
   OZCR2008                              = False;
 
-  TR4W_CURRENTVERSION_NUMBER            = '4.46.3' ;     // N4AF
+  TR4W_CURRENTVERSION_NUMBER            = '4.46.4' ;     // N4AF
 
   TR4W_CURRENTVERSION                   = 'TR4W v.' + TR4W_CURRENTVERSION_NUMBER;//{$IF LANG <> 'ENG'} + ' [' + LANG + ']'{$IFEND}{$IF MMTTYMODE} + '_mmtty'{$IFEND};
 
-  TR4W_CURRENTVERSIONDATE               = 'Feb 24, 2016' ;
+  TR4W_CURRENTVERSIONDATE               = 'Feb 25, 2016' ;
 
   TR4WSERVER_CURRENTVERSION             = '1.41';
 
