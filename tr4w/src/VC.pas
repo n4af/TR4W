567a8de0751dc749284ac55b525155e46300db6b
Howie N4AF <Howie.N4AF@gmail.com>
2016-12-28 07:09:19 -0500

    4.55.2 issue 219

--- tr4w/src/VC.pas
+++ tr4w/src/VC.pas
@@ -175,11 +175,11 @@ const
 
   OZCR2008                              = False;
 
-  TR4W_CURRENTVERSION_NUMBER            = '4.55.1';     // N4af     New Release
+  TR4W_CURRENTVERSION_NUMBER            = '4.55.2';     // N4af     New Release
 
 
   TR4W_CURRENTVERSION                   = 'TR4W v.' + TR4W_CURRENTVERSION_NUMBER; //  {$IF MMTTYMODE} + '_mmtty'{$IFEND};//{$IF LANG <> 'ENG'} + ' [' + LANG + ']'{$IFEND}{$IF MMTTYMODE} + '_mmtty'{$IFEND};
-  TR4W_CURRENTVERSIONDATE               = 'December 18, 2016' ;
+  TR4W_CURRENTVERSIONDATE               = 'December 28, 2016' ;
 
   TR4WSERVER_CURRENTVERSION             = '1.41';
 
