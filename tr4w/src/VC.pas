0c1c27602faa51ede9ffbf02cd62447816cc7f15
Howie N4AF <Howie.N4AF@gmail.com>
2018-03-13 14:09:08 -0400

    issue 336

--- tr4w/src/VC.pas
+++ tr4w/src/VC.pas
@@ -176,11 +176,11 @@ const
   OZCR2008                              = False;
 
 
-  TR4W_CURRENTVERSION_NUMBER            = '4.69.3' ;  // N4af     New Release
+  TR4W_CURRENTVERSION_NUMBER            = '4.69.4' ;  // N4af     New Release
 
 
   TR4W_CURRENTVERSION                   = 'TR4W v.' + TR4W_CURRENTVERSION_NUMBER; //  {$IF MMTTYMODE} + '_mmtty'{$IFEND};//{$IF LANG <> 'ENG'} + ' [' + LANG + ']'{$IFEND}{$IF MMTTYMODE} + '_mmtty'{$IFEND};
-  TR4W_CURRENTVERSIONDATE               = 'March 12, 2018' ;
+  TR4W_CURRENTVERSIONDATE               = 'March 13, 2018' ;
 
  
   TR4WSERVER_CURRENTVERSION             = '1.41';
