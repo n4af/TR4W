529dccd6339dd6e32496df5152aa8160cac31d47
unknown <n4af@n4af.net>
2018-12-30 12:35:55 -0500

    4.76.6 STEW-PERRY filename

--- tr4w/src/VC.pas
+++ tr4w/src/VC.pas
@@ -146,7 +146,7 @@ const
   ICOM_LONG_MODECOMMAND                 = True;
 
 const
-//         LANG                                  = 'ENG';
+        LANG                                  = 'ENG';
 //       LANG                                  = 'RUS';
 //       LANG                                  = 'SER';
 //       LANG                                  = 'ESP';
@@ -181,11 +181,11 @@ const
 
 
 
-  TR4W_CURRENTVERSION_NUMBER            = '4.76.5' ;  // N4af     New Release
+  TR4W_CURRENTVERSION_NUMBER            = '4.76.6' ;  // N4af     New Release
 
 
   TR4W_CURRENTVERSION                   = 'TR4W v.' + TR4W_CURRENTVERSION_NUMBER; //  {$IF MMTTYMODE} + '_mmtty'{$IFEND};//{$IF LANG <> 'ENG'} + ' [' + LANG + ']'{$IFEND}{$IF MMTTYMODE} + '_mmtty'{$IFEND};
-  TR4W_CURRENTVERSIONDATE               = 'Dec 22, 2018' ;
+  TR4W_CURRENTVERSIONDATE               = 'Dec 30, 2018' ;
 
 
 
