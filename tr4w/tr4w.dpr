ddd3ca1402ba31d265390a6a0bad707585d99dab
unknown <n4af@n4af.net>
2019-02-06 19:00:01 -0500

    4.78.0 issue 373

--- tr4w/tr4w.dpr
+++ tr4w/tr4w.dpr
@@ -767,25 +767,22 @@ begin
                 end;
 }
             end;
-            if ((Msg.wParam = VK_LEFT) or (Msg.wParam = VK_RIGHT))   then          // 4.70.2
-           begin
-
-//              if Msg.lParam = 2752513 then SendStringAndStop('E');
+  if Msg.wParam = VK_SHIFT  then
+            begin
               if ShiftKeyEnable then
               begin
-               if   (Msg.wParam = VK_LEFT)  then if OpMode = CQOpMode then  // 4.70.2
+                if lobyte(HiWord(Msg.lParam)) = 42 then if OpMode = CQOpMode then
                 begin
                 RITBumpDown; VFOBumpDown;
                 end;
-               if   (Msg.wParam = VK_RIGHT)  then if OpMode = CQOpMode then        // 4.70.2
+                if lobyte(HiWord(Msg.lParam)) = 54 then if OpMode = CQOpMode then
                 begin
                 RITBumpUp; VFOBumpUp;
                 end;
-               end;
+              end;
             end;
           end;
         end;
-
       WM_KEYUP:
         begin
           if (Msg.wParam = VK_CONTROL) or (Msg.wParam = VK_MENU) then ShowFMessages(0);
