f02a65dbb301ebc9f4fbde0ff7fa8b7eb29e1ec0
Howie N4AF <n4af@n4af.net>
2019-06-30 14:51:11 -0400

    4.82.0 July 2019

--- tr4w/src/VC.pas
+++ tr4w/src/VC.pas
@@ -146,14 +146,14 @@ const
   ICOM_LONG_MODECOMMAND                 = True;
 
 const
-       LANG                                  = 'ENG';
+//       LANG                                  = 'ENG';
 //       LANG                                  = 'RUS';
 //       LANG                                  = 'SER';
 //       LANG                                  = 'ESP';
 //       LANG                                  = 'MNG';
 //       LANG                                  = 'POL';
 //       LANG                                  = 'CZE';
-//       LANG                                  = 'ROM';
+       LANG                                  = 'ROM';
 //       LANG                                  = 'CHN';
 
 {$IF LANG = 'ENG'}{$INCLUDE lang\tr4w_consts_eng.pas}{$IFEND}
@@ -181,11 +181,11 @@ const
 
 
 
-  TR4W_CURRENTVERSION_NUMBER            = '4.80.1' ;  // N4af     New Release
+  TR4W_CURRENTVERSION_NUMBER            = '4.82.0' ;  // N4af     New Release
 
 
   TR4W_CURRENTVERSION                   = 'TR4W v.' + TR4W_CURRENTVERSION_NUMBER; //  {$IF MMTTYMODE} + '_mmtty'{$IFEND};//{$IF LANG <> 'ENG'} + ' [' + LANG + ']'{$IFEND}{$IF MMTTYMODE} + '_mmtty'{$IFEND};
-  TR4W_CURRENTVERSIONDATE               = 'April 10, 2019' ;
+  TR4W_CURRENTVERSIONDATE               = 'July 1, 2019' ;
 
 
 
@@ -1018,6 +1018,7 @@ type
     IC7850,
     IC7851,
     IC9100,
+    IC9700,
 
     OMNI6,
     Orion);
