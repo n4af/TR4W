ab8eacc1dca9bdadd42c6adab46646b59a589014
Howie N4AF <n4af@n4af.net>
2020-08-10 11:05:44 -0400

    additional debug BetaAug10

--- tr4w/src/VC.pas
+++ tr4w/src/VC.pas
@@ -181,11 +181,11 @@ const
 
 
 
-  TR4W_CURRENTVERSION_NUMBER            = '4.91.3' ;  // N4af     New Release
+  TR4W_CURRENTVERSION_NUMBER            = '4.91.betaAug10' ;  // N4af     New Release
 
 
   TR4W_CURRENTVERSION                   = 'TR4W v.' + TR4W_CURRENTVERSION_NUMBER; //  {$IF MMTTYMODE} + '_mmtty'{$IFEND};//{$IF LANG <> 'ENG'} + ' [' + LANG + ']'{$IFEND}{$IF MMTTYMODE} + '_mmtty'{$IFEND};
-  TR4W_CURRENTVERSIONDATE               = 'August 7, 2020' ;
+  TR4W_CURRENTVERSIONDATE               = 'August 10, 2020' ;
 
   TR4WSERVER_CURRENTVERSION             = '1.41';
 
