22059516005d6f2fa5eaa3a4280f36009a363d50
Howie N4AF <n4af@n4af.net>
2020-08-13 12:46:12 -0400

    digital mode ssb radios 4.91.7

--- tr4w/src/VC.pas
+++ tr4w/src/VC.pas
@@ -184,12 +184,12 @@ const
 
 
 
-  TR4W_CURRENTVERSION_NUMBER            = '4.91.6' ;  // N4af     New Release
+  TR4W_CURRENTVERSION_NUMBER            = '4.91.7' ;  // N4af     New Release
 
 
 
   TR4W_CURRENTVERSION                   = 'TR4W v.' + TR4W_CURRENTVERSION_NUMBER; //  {$IF MMTTYMODE} + '_mmtty'{$IFEND};//{$IF LANG <> 'ENG'} + ' [' + LANG + ']'{$IFEND}{$IF MMTTYMODE} + '_mmtty'{$IFEND};
-  TR4W_CURRENTVERSIONDATE               = 'August 12, 2020' ;
+  TR4W_CURRENTVERSIONDATE               = 'August 13, 2020' ;
 
   TR4WSERVER_CURRENTVERSION             = '1.41';
 
