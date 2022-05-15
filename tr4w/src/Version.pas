12bf622682136988fbfbb5bd7782fb54cd8df2e3
Howie N4AF <n4af@n4af.net>
2022-05-15 13:43:54 -0400

    issue 599/600 4.111.2

--- tr4w/src/Version.pas
+++ tr4w/src/Version.pas
@@ -20,7 +20,7 @@ interface
 
 
 const
- TR4W_CURRENTVERSION_NUMBER            = '4.111.0' ;  // N4af     New Release
+ TR4W_CURRENTVERSION_NUMBER            = '4.111.1' ;  // N4af     New Release
 
   TR4W_CURRENTVERSION                   = 'TR4W v.' + TR4W_CURRENTVERSION_NUMBER; //  {$IF MMTTYMODE} + '_mmtty'{$IFEND};//{$IF LANG <> 'ENG'} + ' [' + LANG + ']'{$IFEND}{$IF MMTTYMODE} + '_mmtty'{$IFEND};
   TR4W_CURRENTVERSIONDATE               = 'May, 2022' ;
