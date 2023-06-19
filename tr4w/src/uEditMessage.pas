dd0e94fcf69f5e6d8d782a2f15c3e9cc4c0ed13b
Howie N4AF <n4af@n4af.net>
2023-06-19 18:24:56 -0400

    remove 4.123.4 in 4.123.6

--- tr4w/src/uEditMessage.pas
+++ tr4w/src/uEditMessage.pas
@@ -79,7 +79,7 @@ uses uCFG,
 function EditMessageDlgProc(hwnddlg: HWND; Msg: UINT; wParam: wParam; lParam:
   lParam): BOOL; stdcall;
 label
-  1 , 2;
+  1 ;
 var
   i: Cardinal;
   ID: Str80;
@@ -200,32 +200,12 @@ begin
               //              ShowSysErrorMessage('PLAY FILE');
             end;
 
-          2: goto 1;
+         
           1: //if not PutCommandFromHintListBox then
             begin
               ID[0] := Char(Windows.GetDlgItemText(hwnddlg, 101, @ID[1], 80));
-              CMD[0] := Char(Windows.GetDlgItemText(hwnddlg, 102, @CMD[1],
-                255));
-                if (pos(#1,CMD)<> 0) then  goto 2
-                 else
-                 begin
-                  if (pos(#2,CMD) <> 0) then  goto 2
-                   else
-                   begin
-                    if (pos(#3,CMD) <> 0) then goto 2
-                     else
-                     begin
-                      if(pos(#6,CMD) <> 0) then goto 2
-                       else
-                       begin
-                        if(pos('_',CMD) <> 0) then goto 2
-                         else
-                         CMD := '_' + CMD;
-                    end;
-                      end;
-                       end;
-                        end;
-         2:  DeleteEscapeChars(CMD);
+              CMD[0] := Char(Windows.GetDlgItemText(hwnddlg, 102, @CMD[1], 255));
+              DeleteEscapeChars(CMD);
               Windows.WritePrivateProfileString(m, @ID[1], @CMD{@CMD[1]},
                 @TR4W_CFG_FILENAME);
               CheckCommand(@ID, CMD);
