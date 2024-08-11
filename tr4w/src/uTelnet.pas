06ee71632ea31824f6b660c824ab6bc7ce8dd1b6
Howie N4AF <n4af@n4af.net>
2024-08-11 15:48:08 -0400

    4.137 Aug 2024

--- tr4w/src/uTelnet.pas
+++ tr4w/src/uTelnet.pas
@@ -294,7 +294,7 @@ begin
         //   showmessage(@TelnetBuffer);
 
         TelnetBuffer[i] := #0;
-     {   spotcnt := spotcnt + 1;       // beta test alleviate # screen writes
+        spotcnt := spotcnt + 1;       // beta test alleviate # screen writes
         if spotcnt = 1 then
         begin
           move(TelnetBuffer[0], SpotsBuffer[0], i);
@@ -305,10 +305,10 @@ begin
         begin
           spotcnt := 0;
           ProcessTelnetString(i);
-          move(SpotsBuffer[0], TelnetBuffer[0], i); }
+          move(SpotsBuffer[0], TelnetBuffer[0], i);
           ProcessTelnetString(i);
 
-      //  end;                               // end beta
+       end;                               // end beta
 
         //Except on E : Exception do
        //    begin
